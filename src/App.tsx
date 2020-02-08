1a09643cacbc6c9befeb9a82c5f282b93bc9bca3
tterimaa <tterimaa@gmail.com>
2020-02-08 13:06:09 +0200

    UbuntuMono font + styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Restaurants from './Restaurants'
 import Layout from './Layout'
-import './css/global.css'
 
 const App: React.FC = () => {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import './fonts/UbuntuMono-Regular.ttf'
+import './css/index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
