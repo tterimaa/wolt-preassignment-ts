d79699d6c0ec4096f6bdb8969aeec8c452f19037
tterimaa <tterimaa@gmail.com>
2020-02-08 14:23:44 +0200

    Show description and price on hover

--- src/RestaurantItem.tsx
+++ src/RestaurantItem.tsx
@@ -15,16 +15,14 @@ const RestaurantItem: React.FC<Restaurant> = ({ name, image, city, delivery_pric
         <h2>{name}</h2>
         <span>{city}</span>
       </div>
-      <div className={styles.description}>
+      <div className={styles.info}>
         <h3>{description}</h3>
+        <p>{`Cheap delivery: ${formattedPrice}`}</p>
       </div>
       <div className={styles.tags}>
         {tags.map(tag => (
           <div key={tag}>{tag} </div>
         ))}
-      </div>
-      <div className={styles.info}>
-        <p>{`Cheap delivery: ${formattedPrice}`}</p>
       </div>
     </div>
   )
